fc05f42336c7928e7c1d98c8a5d121dc5e5b7c95
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-01 04:45:24 +0300

    handle task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars, no-shadow
     const task = true;
+    // eslint-disable-next-line no-unused-vars, no-shadow
     const task2 = false;
   }
-  /* eslint-enable no-unused-vars */
 
   return [task, task2];
 }
