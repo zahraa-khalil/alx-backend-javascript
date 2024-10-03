35fdd410f499d0a05af010db33b982495c3f364c
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-04 02:03:49 +0300

    t1.2

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,6 +4,5 @@ export default function initializeRooms() {
   const room1 = new ClassRoom(19);
   const room2 = new ClassRoom(20);
   const room3 = new ClassRoom(34);
-
-  return [room1, room2, room3]
+  return [room1, room2, room3];
 }
