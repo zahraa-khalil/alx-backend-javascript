a0454806c165e21860edc77eacc781f16a8807c0
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-04 02:01:02 +0300

    t3.2

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,13 +3,21 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
-  get code() { return this._code; }
-  get name() { return this._name; }
+  get code() {
+    return this._code;
+  }
+  get name() {
+    return this._name;
+  }
 
-  set code(value) { this._code = value; }
-  set name(value) { this._name = value; }
+  set code(value) {
+    this._code = value;
+  }
+  set name(value) {
+    this._name = value;
+  }
 
   displayFullCurrency() {
-    return this.name + ` (${this.code})`
+    return this.name + ` (${this.code})`;
   }
 }
