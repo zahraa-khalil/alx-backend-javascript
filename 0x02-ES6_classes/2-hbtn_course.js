9e1dc4b7cd45ecf51c92458e8ad1d6c9de1ac25e
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-04 01:36:40 +0300

    t2.1

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,15 +1,39 @@
 export default class HolbertonCourse {
+
   constructor(name, length, students) {
     this._name = name;
     this._length = length;
     this._students = students;
   }
-  
+
   get name() { return this._name; }
 
   get length() { return this._length; }
 
   get students() { return this._students; }
 
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new Error('Name must be a string');
+    }
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number' && newLength > 0) {
+      this._length = newLength;
+    } else {
+      throw new Error('Length must be a number');
+    }
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents) && newStudents.every(student => typeof student === 'string')) {
+      this._students = newStudents;
+    } else {
+      throw new Error('Students must be an array of strings');
+    }
+  }
 }
 
