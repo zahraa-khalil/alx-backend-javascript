9c03725e4803b91dd783c0315110ae2b3939a168
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-04 03:15:33 +0300

    t5.1

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,13 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-
-    if (typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
