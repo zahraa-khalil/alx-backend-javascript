a228a2fc793af9986446ca86f4f4e3c2f20c9133
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-04 03:31:27 +0300

    t7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,34 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('Name must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new Error('Code must be a string');
+    }
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
