7b4825165e3214f0fbbc2edf36a1b13399518ee5
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-11-27 03:56:32 +0200

    update-1.1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,9 @@
-// Import the process module
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for user input
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim(); // Convert input to a string and remove extra spaces/newlines
-  console.log(`Your name is: ${name}`);
-  
-  // End the process after displaying the message
-  process.stdin.end(); 
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-// Handle the end of input
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
